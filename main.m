a003d307be16682e506707d8a316d2043df3570c
Francesco Silvi <fra.silvi1998@outlook.it>
2021-05-28 10:49:48 +0200

    update

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ global l d
 l=1;
 d=0.5;
 
-Q=[1*l;1.5*l;pi/4];
+Q=[2*l;1*l;pi/2+0.01];
 
 P=kin_dir_pos(Q)
 
@@ -14,3 +14,9 @@ plot([0 Q(1)],[0 0],'r')
 hold on
 plot([Q(1) Q(1)+l*cos(Q(3))],[0 l*sin(Q(3))])
 plot([Q(1)+l*cos(Q(3)) Q(1)+l*cos(Q(3))+l*cos(P(3)+pi/2)],[l*sin(Q(3)) l*sin(Q(3))+l*sin(P(3)+pi/2)])
+plot([Q(1)+l*cos(Q(3))+l*cos(P(3)+pi/2) 0],[l*sin(Q(3))+l*sin(P(3)+pi/2) Q(2)])
+
+axis equal
+
+
+

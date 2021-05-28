870ebe74b6c4dfb1cea22690fea4f1010f2c3c4a
Ado <adocafaro@gmail.com>
2021-05-28 12:49:00 +0200

    Update main.m

--- main.m
+++ main.m
@@ -16,6 +16,8 @@ plot([Q(1) Q(1)+l*cos(Q(3))],[0 l*sin(Q(3))])
 plot([Q(1)+l*cos(Q(3)) Q(1)+l*cos(Q(3))+l*cos(P(3)+pi/2)],[l*sin(Q(3)) l*sin(Q(3))+l*sin(P(3)+pi/2)])
 plot([Q(1)+l*cos(Q(3))+l*cos(P(3)+pi/2) 0],[l*sin(Q(3))+l*sin(P(3)+pi/2) Q(2)])
 
+plot([Q(1)+l*cos(Q(3))+l/2*cos(P(3)+pi/2) P(1)],[l*sin(Q(3))+l/2*sin(P(3)+pi/2) P(2)])
+plot(P(1),P(2),'o')
 axis equal
 
 

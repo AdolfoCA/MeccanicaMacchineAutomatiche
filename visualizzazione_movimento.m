0ebf2705a893727827da4c0688a50749b1bd8697
Francesco Silvi <fra.silvi1998@outlook.it>
2021-06-08 10:42:08 +0200

    update

--- visualizzazione_movimento.m
+++ visualizzazione_movimento.m
@@ -9,9 +9,11 @@ P=out.simout1.Data(:,:);
 
 % Grafica
 for i=1:10:length(Q)
-figure(1)    
-plot([Q(i,1) Q(i,1)+l*cos(Q(i,3))],[0 l*sin(Q(i,3))],'-o')
+figure(1)
+plot([0 0],[0 2],'-sk','LineWidth',2)   %pattino
 hold on
+plot([0 2],[0 0],'-sk','LineWidth',2)   %pattino
+plot([Q(i,1) Q(i,1)+l*cos(Q(i,3))],[0 l*sin(Q(i,3))],'-o')
 plot([Q(i,1)+l*cos(Q(i,3)) Q(i,1)+l*cos(Q(i,3))+l*cos(P(i,3)+pi/2)],[l*sin(Q(i,3)) l*sin(Q(i,3))+l*sin(P(i,3)+pi/2)],'-o')
 plot([Q(i,1)+l*cos(Q(i,3))+l*cos(P(i,3)+pi/2) 0],[l*sin(Q(i,3))+l*sin(P(i,3)+pi/2) Q(i,2)],'-o')
 plot([Q(i,1)+l*cos(Q(i,3))+l/2*cos(P(i,3)+pi/2) P(i,1)],[l*sin(Q(i,3))+l/2*sin(P(i,3)+pi/2) P(i,2)],'-')
@@ -23,6 +25,7 @@ plot(Q(i,1),0,'.','MarkerSize',40)
 plot(0,Q(i,2),'sb','MarkerSize',40)
 xlim([-0.1 2])
 ylim([-0.1 2])
+axis equal
 grid on
 drawnow
 hold off
